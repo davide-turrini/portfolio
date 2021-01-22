f10c0044c6204a4e1d87e693a7d1e2066b1e355d
Davide <dt.turrini@gmail.com>
2021-01-22 14:15:21 +0100

    including nuxt sub

---
+++ .nuxt/nuxt-i18n/options.js
@@ -0,0 +1,29 @@
+export const vueI18n = {"fallbackLocale":"en","messages":{"en":{"seo":{"title":"Davide Turrini | Telecommunications Engineering | My portfolio","description":"Hi and welcome to my portfolio, my name is Davide, would you collaborate with me?"},"skip":"skip intro","at":"at","onDate":"on date","wrote":"wrote","experience":"Experience","education":"Education","skills":"Skills"},"it":{},"fr":{},"de":{}}}
+export const vueI18nLoader = false
+export const locales = [{"code":"en","iso":"en-US","file":"~i18n/en-US.js","name":"English"},{"code":"it","iso":"it","file":"~i18n/it.js","name":"Italiano"},{"code":"fr","iso":"fr","file":"~i18n/fr.js","name":"Français"},{"code":"de","iso":"de","file":"~i18n/de.js","name":"Deutsche"}]
+export const defaultLocale = 'en'
+export const routesNameSeparator = '___'
+export const defaultLocaleRouteNameSuffix = 'default'
+export const strategy = 'prefix_except_default'
+export const lazy = false
+export const langDir = null
+export const rootRedirect = null
+export const detectBrowserLanguage = {"useCookie":true,"cookieCrossOrigin":false,"cookieDomain":null,"cookieKey":"i18n_redirected","cookieSecure":false,"alwaysRedirect":false,"fallbackLocale":"","onlyOnNoPrefix":false,"onlyOnRoot":false}
+export const differentDomains = false
+export const seo = false
+export const baseUrl = ''
+export const vuex = {"moduleName":"i18n","syncLocale":false,"syncMessages":false,"syncRouteParams":true}
+export const parsePages = true
+export const pages = {}
+export const beforeLanguageSwitch = () => null
+export const onLanguageSwitched = () => null
+export const IS_UNIVERSAL_MODE = false
+export const MODULE_NAME = 'nuxt-i18n'
+export const LOCALE_CODE_KEY = 'code'
+export const LOCALE_ISO_KEY = 'iso'
+export const LOCALE_DOMAIN_KEY = 'domain'
+export const LOCALE_FILE_KEY = 'file'
+export const STRATEGIES = {"PREFIX":"prefix","PREFIX_EXCEPT_DEFAULT":"prefix_except_default","PREFIX_AND_DEFAULT":"prefix_and_default","NO_PREFIX":"no_prefix"}
+export const COMPONENT_OPTIONS_KEY = 'nuxtI18n'
+export const localeCodes = ["en","it","fr","de"]
+export const trailingSlash = undefined
