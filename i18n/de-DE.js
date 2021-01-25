1d7645457433bddc3d4309eab7976655f4ae650a
Davide <dt.turrini@gmail.com>
2021-01-25 11:37:06 +0100

    FIX le traduzioni

--- i18n/de-DE.js
+++ i18n/de-DE.js
@@ -1,7 +1,7 @@
 export default {
   seo: {
     title: 'Davide Turrini | Telekommunikationstechnik | Mein Portfolio',
-    description: 'uchen Sie eine Zusammenarbeit? Willkommen in meinem Portfolio!'
+    description: 'Suchen Sie eine Zusammenarbeit? Willkommen in meinem Portfolio!'
   },
   skip: 'Intro überspringen',
   at: 'beim',
--- i18n/en-US.js
+++ i18n/en-US.js
@@ -1,7 +1,7 @@
 export default {
   seo: {
     title: 'Davide Turrini | Telecommunications Engineering | My portfolio',
-    description: 'Hi and welcome to my portfolio, my name is Davide, would you collaborate with me?'
+    description: 'Hi and welcome to my portfolio, my name is Davide, are you interested in collaborating with me?'
   },
   skip: 'skip intro',
   at: 'at',
