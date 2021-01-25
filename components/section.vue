e41a3512afda71fd41fa9651d053334b8ea3a3d8
Davide <dt.turrini@gmail.com>
2021-01-25 11:38:35 +0100

    fix sezioni

--- components/section.vue
+++ components/section.vue
@@ -2,7 +2,7 @@
   <section class="mx-5 md:mx-16">
     <div class="w-full bg-awesome-gradient mt-10" style="padding: 1px 0;">
       <div class="bg-gray-900">
-        <h3 class="w-full text-center text-2xl tracking-widest font-light py-4 bg-clip-text text-transparent bg-awesome-gradient">
+        <h3 class="w-full text-center text-2xl tracking-widest font-light py-4 bg-clip-text text-transparent bg-awesome-gradient capitalize">
           {{ title }}
         </h3>
       </div>
